06a2a6895e559719604786c2c4ecdad13c401127
polar <derekjoel418@gmail.com>
2026-02-12 20:04:30 -0600

    Remove AI functionality and associated configurations

--- src/config/settings.js
+++ src/config/settings.js
@@ -5,6 +5,5 @@ module.exports = {
     ownerNumber: "521234567890", // Example, change it
     githubUrl: "github.com/Joe-ATC",
     sessionPath: "auth_info_baileys",
-    version: "2.1.0",
-    geminiApiKey: "TU_API_KEY_AQUI" // Reemplaza esto con tu clave de Google AI Studio
+    version: "2.1.0"
 };
